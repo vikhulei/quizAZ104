ad894322a3543e24ad3edfbe5b40c7161277616e
vikhulei <vikhulei@gmail.com>
2024-04-30 20:01:20 +0100

    questions corrected

--- src/Questions.js
+++ src/Questions.js
@@ -1,10 +1,10 @@
 const Questions = {
   ADDS: [
     {q: "	Three types of user accounts	", a: "	1) Cloud<br>2) Directory synchronized<br>3) Guest user	", tag: 0},
-    {q: "	Two teplates to create cloud user	", a: "	1) Create user<br>2) Invite user	", tag: 0},
-    {q: "	Wjo can preset data for the cloud user?	", a: "	Global administrator or User administrator	", tag: 0},
-    {q: "	Which two names do the accounts have?	", a: "	1) Displaty name - Viktor Hulei<br>User account name vikhulei@fixitfast.com	", tag: 0},
-    {q: "	How many days before cloud user account is permanentrly deleted?	", a: "	30 days	", tag: 0},
+    {q: "	Two templates to create cloud user	", a: "	1) Create user<br>2) Invite user	", tag: 0},
+    {q: "	Who can preset data for the cloud user?	", a: "	Global administrator or User administrator	", tag: 0},
+    {q: "	Which two names do the accounts have?	", a: "	1) Display name - Viktor Hulei<br>User account name vikhulei@fixitfast.com	", tag: 0},
+    {q: "	How many days before cloud user account is permanently deleted?	", a: "	30 days	", tag: 0},
     {q: "	How to view errors when creating bulk accounts?	", a: "	Bulk operation results page in the Azure portal	", tag: 0},
   ]
 };
