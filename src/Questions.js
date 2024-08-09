2a2b5352234dc498c4454fc0479d287b9f858eba
vikhulei <65869915+vikhulei@users.noreply.github.com>
2024-08-09 15:28:59 +0100

    Update Questions.js

--- src/Questions.js
+++ src/Questions.js
@@ -1,6 +1,8 @@
 const Questions = {
   Exam: [
     {q: "Which record to add to verify the custom domain?", a: "TXT", tag: 0},
+    {q: "1) What is pass-through authentication?<br>2) Seamless SSO?<br>3) Password hash synchronization?", a: "1) Check credentials against on-prem AD<br>2) No creds required once logged in<br>3) The password hash is hashed and synchronized to Microsoft Entra ID from on-prem AD", tag: 0},
+    {q: "  ", a: "  ", tag: 0},
   ]
 };
 
